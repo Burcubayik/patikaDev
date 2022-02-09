388999aa94333e02244b6894e50550a72104b85e
Burcubayik <92742235+Burcubayik@users.noreply.github.com>
2022-02-09 23:01:51 +0300

    manav kasa programı

---
+++ patikaDevOdevler/src/manavKasa/Main.java
@@ -0,0 +1,34 @@
+package manavKasa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double armut=2.14, elma=3.67, domates=1.11, muz=0.95,patlican=5.00, toplam;
+		int armutKilo,elmaKilo,domatesKilo,muzKilo,patlicanKilo;
+		
+		Scanner input=new Scanner(System.in);
+		
+		System.out.print("Armut kaç kilo? ");
+		armutKilo=input.nextInt();
+		
+		System.out.print("Elma kaç kilo? ");
+		elmaKilo=input.nextInt();
+		
+		System.out.print("Domates kaç kilo? ");
+		domatesKilo=input.nextInt();
+		
+		System.out.print("Muz kaç kilo? ");
+		muzKilo=input.nextInt();
+		
+		System.out.print("Patlýcan kaç kilo? ");
+		patlicanKilo=input.nextInt();
+		
+		toplam= (armut*armutKilo)+ (elma*elmaKilo) + (domates*domatesKilo) + (muz* muzKilo) + (patlican*patlicanKilo);
+		
+		System.out.print("Toplam tutar: " + toplam+ " TL");
+
+	}
+
+}
