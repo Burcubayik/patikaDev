397091bc7ce98b66b9ee159c2644591ee4a73d3a
Burcubayik <92742235+Burcubayik@users.noreply.github.com>
2022-02-17 11:35:14 +0300

    added harmonic series

---
+++ patikaDevOdevler/src/harmonicSeries/Main.java
@@ -0,0 +1,21 @@
+package harmonicSeries;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Sayý giriniz: ");
+		int n=input.nextInt();
+		double result=0;
+		
+		for(double i=1; i<=n ;i++) {
+			
+			result+=(1/i);
+			
+		}
+		System.out.println(result);
+	}
+
+}
